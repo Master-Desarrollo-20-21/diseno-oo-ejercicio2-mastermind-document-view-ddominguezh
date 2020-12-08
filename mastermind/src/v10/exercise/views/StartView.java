47d1ad8657ca61907beb15a640f1fd414db17fb7
ddominguezh <diegoalbert500@gmail.com>
2020-12-08 21:19:35 +0100

    Update StartView.java

--- mastermind/src/v10/exercise/views/StartView.java
+++ mastermind/src/v10/exercise/views/StartView.java
@@ -3,7 +3,7 @@ package v10.exercise.views;
 import v10.exercise.utils.Console;
 import v10.exercise.utils.Message;
 
-public class StartView extends SubView{
+public class StartView extends AbstractView{
 
 	public void interact() {
 		Console.getInstance().write(Message.START);
